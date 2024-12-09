e9c61096aca8b9110ea1b12bdf2d27d766b660df
Balint Valoczy <balint.valoczy@bd-orange.com>
2024-12-09 16:13:25 +0100

    Chunking

--- src/main/java/com/orange/ai_worskhop/service/BookService.java
+++ src/main/java/com/orange/ai_worskhop/service/BookService.java
@@ -1,8 +1,13 @@
 package com.orange.ai_worskhop.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import com.orange.ai_worskhop.domain.Metadata;
@@ -20,5 +25,33 @@ public class BookService {
         String language = container.select("p:contains(Language)").text().replace("Language: ", "");
 
         return new Metadata(title, author, releaseDate, language);
+    }
+
+    public List<String> extractChunks(String html) {
+        List<String> paragraphs = extractParagraphs(html);
+        List<String> chunks = new ArrayList<>();
+        StringBuilder currentChunk = new StringBuilder();
+
+        for (String paragraph : paragraphs) {
+            if (currentChunk.length() + paragraph.length() >= 500 && currentChunk.length() > 0) {
+                chunks.add(currentChunk.toString());
+                currentChunk = new StringBuilder();
+            }
+            currentChunk.append(paragraph).append(" ");
+        }
+
+        if (currentChunk.length() > 0) {
+            chunks.add(currentChunk.toString());
+        }
+
+        return chunks;
+    }
+    
+    public List<String> extractParagraphs(String html) {
+        Document document = Jsoup.parse(html);
+        Elements paragraphs = document.select("p");
+        return paragraphs.stream()
+                .map(Element::text)
+                .collect(Collectors.toList());
     }
 }
